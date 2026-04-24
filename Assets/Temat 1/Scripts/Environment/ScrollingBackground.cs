ff248e687d332f8ef525100f67a2b742c153522c
Rockaa <jakara1093@gmail.com>
2026-04-24 17:38:46 +0200

    update

---
+++ Assets/Temat 1/Scripts/Environment/ScrollingBackground.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer m_spriteRenderer;
+
+    [SerializeField] private float m_scrollScale = Constants.Background.ScrollScale;
+    private Material m_material;
+    private float m_offset;
+
+    private void Reset()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Awake()
+    {
+        m_material = m_spriteRenderer.material;
+    }
+
+    private void Update()
+    {
+        if (GameManager.Get.CurrentState != GameState.Playing)
+        {
+            return;
+        }
+
+        Scroll();
+    }
+
+    private void Scroll()
+    {
+        float speed = GameManagerData.Get.PipeScrollSpeed * GameManager.Get.GameSpeed;
+        m_offset = (m_offset + speed * m_scrollScale * Time.deltaTime) % 1f;
+        m_material.mainTextureOffset = new Vector2(m_offset, 0f);
+    }
+}
